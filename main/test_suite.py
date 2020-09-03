e06a1f0f637ab308b1f9e76a0e59609856925c71
Junior Rodriguez <miguel.ajr89@gmail.com>
2020-09-03 14:42:15 -0400

    Improve Test

--- main/test_suite.py
+++ main/test_suite.py
@@ -3,14 +3,15 @@ from pyunitreport import HTMLTestRunner
 from assertions_test import AssertionTest
 from search_test import SearchTest
 
-assertions_test = TestLoader().loadTestsFromTestCase(AssertionTest)
-search_test = TestLoader().loadTestsFromTestCase(SearchTest)
+assertions_testing = TestLoader().loadTestsFromTestCase(AssertionTest)
+search_testing = TestLoader().loadTestsFromTestCase(SearchTest)
 
-tests = TestSuite([assertions_test, search_test])
+tests = TestSuite([assertions_testing, search_testing])
 
 kwargs = {
     'output': 'test_suite_folder',
     'report_name': 'report_test_suite'
 }
 
-runner = HTMLTestRunner(**kwargs).run(tests)
+runner = HTMLTestRunner(**kwargs)
+runner.run(tests)
